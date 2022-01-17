a259497fb014dd0812f93496e1bdb1b3c9ecbd6e
hyuk <dlsgur0906@gmail.com>
2022-01-17 09:55:27 +0900

    Counter컴포넌트에 fixed 활용해봄

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,16 +2,16 @@ import React, {Component} from "react";
 class Counter extends Component {
     state = {
         counter: 0,
-        fixed: 1
+        fixed: 2
     }
     handleIncrease = () => {
         this.setState(state => ({
-            counter: state.counter +1
+            counter: state.counter + state.fixed
         }))
     }
     handleDecrease = () => {
         this.setState(state => ({
-            counter: state.counter -1
+            counter: state.counter - state.fixed
         }))
     }
     render() {
@@ -19,8 +19,8 @@ class Counter extends Component {
         return(
             <div>
                 <h1>{counter}</h1>
-                <button onClick={this.handleIncrease}>+1</button>
-                <button onClick={this.handleDecrease}>-1</button>
+                <button onClick={this.handleIncrease}>+{fixed}</button>
+                <button onClick={this.handleDecrease}>-{fixed}</button>
                 <p>이렇게도 쓸 수 있어요{fixed}</p>
             </div>
         )
